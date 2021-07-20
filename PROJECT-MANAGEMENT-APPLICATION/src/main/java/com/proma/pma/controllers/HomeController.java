373e5722abbdd56f93b22e5d44ea338f7fa247b0
COREi5 <COREi5@192.168.1.42>
2021-07-20 16:08:54 +0200

    Home

--- PROJECT-MANAGEMENT-APPLICATION/src/main/java/com/proma/pma/controllers/HomeController.java
+++ PROJECT-MANAGEMENT-APPLICATION/src/main/java/com/proma/pma/controllers/HomeController.java
@@ -1,5 +1,27 @@
 package com.proma.pma.controllers;
 
-public class HomeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.proma.pma.dao.ProjectRepository;
+import com.proma.pma.entities.Project;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	ProjectRepository proRepo;
+	
+	@GetMapping("/")
+	public String displayHome(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projectsList", projects);
+		return "home";
+		
+	}
+	
 }
